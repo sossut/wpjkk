a5cfe0e6d37d8896f65a71f8f397eaf4b814512d
sossut <mikaeala@k-d557-mac-17.pk.amk.root>
2022-03-25 15:52:24 +0200

    caca

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,27 @@
-import {Link} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {useUser} from '../hooks/ApiHooks';
 
 const Nav = () => {
+  const {getUser} = useUser();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try {
+      const userData = await getUser(localStorage.getItem('token'));
+      console.log(userData);
+      setLoggedIn(true);
+      navigate('/home');
+    } catch (e) {
+      setLoggedIn(false);
+      navigate('/');
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  console.log(loggedIn);
   return (
     <nav>
       <ul>
