ec6c2d875d570b10934555c437ad8d9c13eb23c0
sossut <mikaeala@k-d557-mac-17.pk.amk.root>
2022-04-08 16:27:48 +0300

    abcdef

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
 const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
-const appID = 'azqsw346';
+const appID = 'hienoapp';
 
 export {baseUrl, mediaUrl, appID};
